8fce1d518efe0e6c79f08985241065f45337e77b
Flo <Flo@DETRITUS>
2023-08-26 11:48:06 +0200

    backup

--- DataTransferObjects/Maps/MapRoomUpdateDto.cs
+++ DataTransferObjects/Maps/MapRoomUpdateDto.cs
@@ -1,9 +1,12 @@
-﻿using System.Drawing;
+﻿using DiceDragon.Shared.ValueObjects;
+using System.Drawing;
 
 namespace DiceDragon.Shared.DataTransferObjects.Maps;
 
 public class MapRoomUpdateDto
 {
+    public string? Name { get; set; }
     public List<Point>? Points { get; set; }
+    public DimensionsF? ViewBox { get; set; }
     public bool? IsActive { get; set; }
 }
