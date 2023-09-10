e417fa48619974a4c79eacfcf077ce69829f1b05
Flo <Flo@DETRITUS>
2023-09-10 11:24:47 +0200

    v0.1.9

---
+++ DataTransferObjects/Images/ImageQueryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DiceDragon.Shared.DataTransferObjects.Images;
+
+public class ImageQueryDto
+{
+    public string? ContentType { get; set; }
+    public string? Name { get; set; }
+    public string? Source { get; set; }
+}
